0c19b7529a59efdd57fbd64b19aa2e703b7f9b02
Shriya_Varnita <ll6348@srmist.edu.in>
2025-04-09 10:40:07 +0530

    Updated use case 1

--- user_registration_problem.py
+++ user_registration_problem.py
@@ -1,18 +1,16 @@
 import re
 class UserRegistrationProblem:
-    def __init__(self,name):
-        self.name = name
-    def check_name(self):
+
+    def check_first_name(self):
         is_true = True
         while is_true:
-            self.name = input("Please enter your first name: ")
-            if re.search('[a-zA-Z]{3,}', self.name):
+            self.first_name = input("Please enter your first name: ")
+            if re.search('^[A-Z][a-zA-Z]{2,}$', self.first_name):
                 print("Name is accepted")
                 is_true = False
             else:
                 print("Name shouch contain only alphabets and atleast 3 characters")
 
-name = input("Please enter your first name(Must only contain alphabets and atleast 3 characters): ")
-user_obj = UserRegistrationProblem(name)
-user_obj.check_name()
+user_obj = UserRegistrationProblem()
+user_obj.check_first_name()
 

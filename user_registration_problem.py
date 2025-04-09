24a3c2fca3b421eb35aa08a9ec22e83ac4c3b760
Shriya_Varnita <ll6348@srmist.edu.in>
2025-04-09 11:13:26 +0530

    Use Case 2

--- user_registration_problem.py
+++ user_registration_problem.py
@@ -13,6 +13,19 @@ class UserRegistrationProblem:
                 is_true = False
             else:
                  print("Invalid Input.")
+    
+    def check_last_name(self):
+        is_true = True
+        while is_true:
+            print("Your first name should Start with capital letter")
+            print("Should contain only alphabets.")
+            print("Atleast must contain 3 characters.")
+            self.last_name = input("Please enter your last name: ")
+            if re.fullmatch('^[A-Z][a-zA-Z]{2,}$', self.last_name):
+                print("Name is accepted")
+                is_true = False
+            else:
+                 print("Invalid Input.")
 
 user_obj = UserRegistrationProblem()
 user_obj.check_first_name()
